37d3787c405fe5271c603b1dba7eb6fa62b8f920
mjgonzalez6 <mjgonzalez6@miuandes.cl>
2017-05-27 21:58:47 -0400

    Bootstrap-Sass instalado

--- proyecto/config/routes.rb
+++ proyecto/config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :categories
   resources :people
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'people#index'
+  root to: 'documents#index'
 end
