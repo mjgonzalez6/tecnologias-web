9554aef11a4917adaf3d7978151e80125e1804e5
ditarud <ditarud@miuandes.cl>
2017-05-27 23:13:37 -0400

    Fix People-users

--- proyecto/config/routes.rb
+++ proyecto/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :people, path: 'auth'
+  devise_scope :person do
+    get 'auth', to: 'devise/registrations#new'
+  end
+  authenticate :person do
   resources :assignments
   resources :managements
   resources :documents
@@ -7,4 +11,5 @@ Rails.application.routes.draw do
   resources :people
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'documents#index'
+end
 end
--- proyecto/db/schema.rb
+++ proyecto/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516170441) do
+ActiveRecord::Schema.define(version: 20170528023407) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer  "document_id"
@@ -48,12 +48,8 @@ ActiveRecord::Schema.define(version: 20170516170441) do
     t.string   "last_name"
     t.string   "email"
     t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -63,10 +59,8 @@ ActiveRecord::Schema.define(version: 20170516170441) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_people_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_people_on_reset_password_token", unique: true
   end
 
 end
