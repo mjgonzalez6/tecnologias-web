2d7941c80490d9f9f22c8cfe714536edf679a401
mjgonzalez6 <mjgonzalez6@miuandes.cl>
2017-06-21 15:45:38 -0400

    Sign_up reparado

--- proyecto/config/routes.rb
+++ proyecto/config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
-    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
-  end
+
   resources :suggestions
   resources :documents
   root to: 'documents#index'
-  devise_for :people #, path: 'auth'
-  #devise_scope :person do
-  #  get 'auth', to: 'devise/registrations#new'
+  devise_for :people, path: 'auth'
+  devise_scope :person do
+    get 'auth', to: 'devise/registrations#new'
 
-  #end
+  end
   authenticate :person do
   resources :assignments
   resources :managements
